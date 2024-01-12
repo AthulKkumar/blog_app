694bef863f68522d392f4d5c5451b47f4dff31a0
AthulKkumar <athulkkumar5@gmail.com>
2024-01-12 19:42:44 +0530

    enh(app): apollo server setup completed

---
+++ client/src/ApolloProvider.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloProvider,
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_API_URL,
+});
+
+console.log(import.meta.env.VITE_API_URL);
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
